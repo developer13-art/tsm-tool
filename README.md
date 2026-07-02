00825cb5ad4c007a68ec6b74a53a3288aa06bfea
unknown <iliyaaabdurrazaq@gmail.com>
2026-07-02 02:54:04 -0700

    new changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# tsm-tool
-tsm tool
--- lib/db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./users";
-export * from "./devices";
-export * from "./mdm-packages";
-export * from "./jobs";
-export * from "./activity";
-export * from "./device-operations";
-export * from "./api-tokens";
